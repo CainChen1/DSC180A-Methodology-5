cb6120abdb2215937fc197e05d237577844ddbd7
CainChen1 <xic023@ucsd.edu>
2022-11-23 05:27:28 -0800

    ma5

---
+++ run.py
@@ -0,0 +1,12 @@
+import sys
+from src.models.train import train
+from src.models.test import test
+
+def main(targets):
+    return 
+
+if __name__ == '__main__':
+    
+    targets = sys.argv[1]
+    main(targets)
+    
